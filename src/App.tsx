41c61781fb23cfbf8a837ffa5c8c71035757268e
gigisoza <sozashviligigi@gmail.com>
2023-08-22 22:40:11 +0400

    Add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import Login from "./pages/Login";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn">Button</button>
+      <Login />
     </div>
   );
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,16 @@
+export default function Login() {
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there 👋🏻</h1>
+          <p className="py-6">
+            Join the conversation, meet new people, and make connections in one
+            shared room.
+          </p>
+          <button className="btn btn-primary">Login With Google</button>
+        </div>
+      </div>
+    </div>
+  );
+}
