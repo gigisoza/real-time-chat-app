6a759127c800caf1aedf5b6feb0c7d521946422c
gigisoza <sozashviligigi@gmail.com>
2023-08-23 19:38:26 +0400

    Add firebase

---
+++ src/firebase.tsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_V0GL4LHIEq7e6C2avNhNvGNeRaShYzY",
+  authDomain: "instantchat-71cbd.firebaseapp.com",
+  projectId: "instantchat-71cbd",
+  storageBucket: "instantchat-71cbd.appspot.com",
+  messagingSenderId: "206478487212",
+  appId: "1:206478487212:web:16fa2423c2537a1278f909",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
